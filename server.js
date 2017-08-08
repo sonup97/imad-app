785669bbcd299fe230682054264e6d9351254703
sonup97 <30662033+sonup97@users.noreply.github.com>
2017-08-08 20:38:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/artical-one', function(req, res){
-    res.send('Artical one req will be serve here soon:');
+   res.sendFile(path.join(__dirname, 'ui', 'artical-one.html'));
 });
 
 app.get('/artical-two', function(req, res){

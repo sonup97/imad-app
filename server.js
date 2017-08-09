959b1b5a9870c6ca212ac58de5610e298613e2b3
sonup97 <30662033+sonup97@users.noreply.github.com>
2017-08-09 20:55:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,66 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articalOne={
+    title:'Artical one sonu pandit',
+    heading:'Artical one',
+    date:'5 sept 2017',
+    content:`<p>
+                This is the content for mine firsr partical This is the content for mine firsr partica0This is the content for mine firsr partical
+                </p>
+                <p>
+                This is the content for mine firsr partical This is the content for mine firsr partica0This is the content for mine firsr partical
+                </p>`
+    
+    
+};
+ function creatTemplate(data){
+     
+var title=data.title;
+var date=data.date;
+var heading=data.heading;
+var content=data.content;
+ 
+var htmlTemplate=`
+<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+       
+       <link href="/ui/style.css" rel="stylesheet" />
+           
+        </head>
+        <body>
+    <div class="container">
+                <div>
+                <a href="/">Home</a>
+            </div>
+            <hr>
+            <h3>
+               ${heading}
+                
+            </h3>
+            <div>
+                ${date}
+            </div>
+            <div>
+               ${content}
+      </div>      </div>
+    
+</html>
+
+`;
+return htmlTemplate;
+
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/artical-one', function(req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'artical-one.html'));
+   res.sendFile(creatTemplate(articalOne));
 });
 
 app.get('/artical-two', function(req, res){
